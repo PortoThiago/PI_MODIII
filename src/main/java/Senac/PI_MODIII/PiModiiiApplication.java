1fe1989860894aedc75a08f0ff7f1f8cb6132e7a
Thiago Oliveira <thiago-porto-oliveira@hotmail.com>
2024-10-05 15:46:10 -0300

    Iniciando Criação do Projeto Integrador

---
+++ src/main/java/Senac/PI_MODIII/PiModiiiApplication.java
@@ -0,0 +1,13 @@
+package Senac.PI_MODIII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiModiiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiModiiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Senac/PI_MODIII/PiModiiiApplicationTests.java
@@ -0,0 +1,13 @@
+package Senac.PI_MODIII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiModiiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
