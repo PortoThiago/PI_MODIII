34a8b3d526d717c1d9b1618f8f28aeccc1478580
Thiago Oliveira <thiago-porto-oliveira@hotmail.com>
2024-10-15 14:01:51 -0300

    Criados Templates simples.

---
+++ src/main/java/Senac/PI_MODIII/controller/SiteController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Senac.PI_MODIII.controller;
+
+/**
+ *
+ * @author thiag
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SiteController {
+   @GetMapping("/")
+    public String index() {
+        return "index"; 
+    }
+    
+    @GetMapping("/cadastroProduto")
+    public String cadastroProduto(){
+        return "cadastroProduto";
+    }
+    
+    @GetMapping("/listaProduto")
+    public String listaProduto(){
+        return "listaProduto";
+    }
+}
